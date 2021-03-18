2cf8cc480394b4f555fb6e9d44b8071e2438c1f0
mullkippe <65645313+mullkippe@users.noreply.github.com>
2021-03-18 17:33:53 +0900

    新規ユーザー登録画面ファイル一式

---
+++ register.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>ユーザー登録完了画面</title>
+</head>
+
+	<body>
+	<p>ユーザー登録が完了しました</p>
+	<a href="./signup_form.php">戻る</a>
+
+</body>
+</html>
